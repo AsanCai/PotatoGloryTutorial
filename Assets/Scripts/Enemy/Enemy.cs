176056e35ebb04be4be297b9ce73fb081fea58dc
AsanCai <969850420@qq.com>
2018-12-27 18:57:03 +0800

    上传修改

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Wander))]
+public class Enemy : MonoBehaviour {
+	[Tooltip("障碍物检测点")]
+	[SerializeField]
+    private Transform FrontCheck;
+
+	private Wander m_Wander;
+	private LayerMask m_LayerMask;
+
+	private void Awake() {
+		m_Wander = GetComponent<Wander>();	
+	}
+     
+	private void Start() {
+		m_LayerMask = LayerMask.GetMask("Obstacle");
+	}
+
+	private void Update () {
+		Collider2D[] frontHits = Physics2D.OverlapPointAll(FrontCheck.position, m_LayerMask);
+
+        if(frontHits.Length > 0) {
+			m_Wander.Flip();
+		}
+	}
+}
---
+++ Assets/Scripts/Enemy/Wander.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wander : MonoBehaviour {
+	[Tooltip("是否朝向右边")]
+	[SerializeField]
+	private bool FacingRight = true;
+
+	[Tooltip("怪物水平移动的速度")]
+	[SerializeField]
+    private float MoveSpeed = 2f;
+
+
+	//用于设置怪物对象的物理属性
+    private Rigidbody2D m_Rigidbody;
+	// 用于保存当前的水平移动速度
+	private float m_CurrentMoveSpeed;
+
+	// 获取组件引用
+    private void Awake() {
+        m_Rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+	// 设置字段的初始值
+	private void Start() {
+		if(FacingRight) {
+			m_CurrentMoveSpeed = MoveSpeed;
+		} else {
+			m_CurrentMoveSpeed = -MoveSpeed;
+		}
+	}
+
+	// 执行和物理相关的代码
+	private void FixedUpdate() {
+		m_Rigidbody.velocity = new Vector2(m_CurrentMoveSpeed, m_Rigidbody.velocity.y);
+	}
+
+	// 转向函数
+	public void Flip() {
+		m_CurrentMoveSpeed *= -1;
+		
+		this.transform.localScale = Vector3.Scale(new Vector3(-1, 1, 1), this.transform.localScale);
+	}
+}
